f48e22a344fed1293b653e89ee72067aa6e254e3
marketingcentralmkt <marketingcentral.mkt@gmail.com>
2026-07-16 12:12:50 -0300

    chore(android): APK v3.29.0 (code 51)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.atlas.servidor"
         minSdk = 24
         targetSdk = 34
-        versionCode = 50
-        versionName = "3.28.0"
+        versionCode = 51
+        versionName = "3.29.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
